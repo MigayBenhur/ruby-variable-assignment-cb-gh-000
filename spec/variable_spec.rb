302dade3419556cf1ef0e978050ecc2fc11318ae
N MIGAYA BENHUR <migayajesus@gmail.com>
2020-07-03 14:35:59 +0000

    Automatically backed up by Learn

--- spec/variable_spec.rb
+++ spec/variable_spec.rb
@@ -2,7 +2,7 @@
 # Code your solution in variable.rb
 describe "./variable.rb" do
 
-it "defined a local variable called greeting and set it equal to '4'" do
+it "defined a local variable called greeting and set it equal to 'Hello World'" do
   greeting = get_variable_from_file('./variable.rb', "greeting")
 
   expect(greeting).to eq("Hello World")
